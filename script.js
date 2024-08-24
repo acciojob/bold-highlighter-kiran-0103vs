2bb3411fed4c844eac8fe8868eca3d7ed01d8bcf
kiran-0103vs <131228987+kiran-0103vs@users.noreply.github.com>
2024-08-24 12:20:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function highlight() {
     //Write your code here
+	const boldElements = document.querySelectorAll('strong');
+
+    // Loop through each <strong> element and change the color to green
+    boldElements.forEach(element => {
+        element.style.color = 'green';
+    });
 
 
 }
@@ -7,6 +13,12 @@ function highlight() {
 
 function return_normal() {
     //Write your code here
+	const boldElements = document.querySelectorAll('strong');
+
+    // Loop through each <strong> element and revert the color to black
+    boldElements.forEach(element => {
+        element.style.color = 'black';
+    });
 
     
 }
